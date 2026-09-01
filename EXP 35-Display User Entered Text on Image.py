c86ebc559370e54391b8f08b87c8204d276165e6
jyoshnaroy <122586723+jyoshnaroy@users.noreply.github.com>
2026-09-01 16:24:43 +0530

    Add functionality to display user text on image

---
+++ EXP 35-Display User Entered Text on Image.py
@@ -0,0 +1,13 @@
+import cv2
+img = cv2.imread("image.jpg")
+text = input("Enter text: ")
+cv2.putText(img, text, (50, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 0, 255),
+            2)
+
+cv2.imshow("Image with Text", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
